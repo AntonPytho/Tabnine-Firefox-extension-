ab848c58f9be9a50ef3c98c0bf3fe9060c1c0c9d
pipx⚡🐜⚡wizard <225081509+mixplorernano-debug@users.noreply.github.com>
2025-09-05 08:21:44 -0600

    Update userinit.sh
    pkg update -y && pkg upgrade -y
    pkg install wget -y
    wget https://raw.githubusercontent.com/sabamdarif/kali-proot/main/setup.sh
    chmod +x setup.sh
    bash setup.sh
    kali
    bash gui.sh
         done
    Now select **XFCE**, **KDE**, **LXDE**, **LXQT** OR **GNOME** Desktop (any one)

--- userinit.sh
+++ userinit.sh
@@ -1,1 +1,11 @@
 #!/system/bin/sh
+
+pkg update -y && pkg upgrade -y
+pkg install wget -y
+wget https://raw.githubusercontent.com/sabamdarif/kali-proot/main/setup.sh
+chmod +x setup.sh
+bash setup.sh
+kali
+bash gui.sh
+     done
+Now select **XFCE**, **KDE**, **LXDE**, **LXQT** OR **GNOME** Desktop (any one)
